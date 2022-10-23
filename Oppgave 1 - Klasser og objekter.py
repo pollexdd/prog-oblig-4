4d2ec735a9f3b65a524f7d10e1dcea5360217dbd
pollekillerxd <76852143+pollexdd@users.noreply.github.com>
2022-10-23 14:32:28 +0200

    Update Oppgave 1 - Klasser og objekter.py

--- Oppgave 1 - Klasser og objekter.py
+++ Oppgave 1 - Klasser og objekter.py
@@ -6,9 +6,8 @@ class filmer:
         self.name = name
         self.year = year
         self.score = score
-
-def get_description(self):
-    return f"{self.name} was released in {self.year} and currently has a score of {self.year}"
+    def get_description(self):
+        return f"{self.name} was released in {self.year} and currently has a score of {self.year}"
 
 film_1 = filmer("Inception","2010","Score: 8.8")
 film_2 = filmer("The Martian","2015","Score: 8.0")
@@ -18,6 +17,7 @@ print(f"{film_1.name} was released in {film_1.year} and currently has a score of
 print(f"{film_2.name} was released in {film_2.year} and currently has a score of {film_2.year}")
 print(f"{film_3.name} was released in {film_3.year} and currently has a score of {film_3.year}")
 
+print()
 
 print(film_1.get_description())
 print(film_2.get_description())
